0bb7bb2d42dbbd2cb15b5b1a83a3fa81a0af39bc
zhanghui <zhanghui@souche.com>
2016-12-28 10:48:25 +0800

    +log

--- src/main/java/com/panda/controller/JsonController.java
+++ src/main/java/com/panda/controller/JsonController.java
@@ -3,6 +3,8 @@ package com.panda.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,11 +12,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class JsonController {
 
+	private static final Logger logger = LoggerFactory.getLogger(JsonController.class);
 	
 	@RequestMapping(value = "/hello")
 	@ResponseBody
 	public Object  getData(){
-		System.out.println("request");
+		logger.info("request");
 		Map<String, String> map = new HashMap<String,String>();
 		map.put("name", "zz");
 		map.put("sex", "男");
