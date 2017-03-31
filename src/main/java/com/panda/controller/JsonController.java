d5e56397043e41ceb9b8d678e2f89e212d56aa7a
zhanghui <zhanghui@souche.com>
2017-03-31 15:20:12 +0800

    +json serialize

--- src/main/java/com/panda/controller/JsonController.java
+++ src/main/java/com/panda/controller/JsonController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
