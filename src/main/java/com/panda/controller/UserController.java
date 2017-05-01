3d2e3faf3a02ba862dbff437aea8ef8c69cef3dd
zhanghui <zhanghui@souche.com>
2017-05-01 21:36:44 +0800

    +velocity resolver view

---
+++ src/main/java/com/panda/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.panda.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+	@RequestMapping("/show")
+	public ModelAndView show(){
+		ModelAndView view = new ModelAndView("index");
+		
+		List<String> userList = new ArrayList<>();
+		userList.add("李磊");
+		userList.add("李白");
+		
+		view.addObject("users", userList);
+		return view;
+	}
+}
