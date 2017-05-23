a84d9a4a673662d50404dc0f813c5dfe0e0c5a5f
zhanghui <zhanghui@souche.com>
2017-05-23 16:14:40 +0800

    update

--- src/main/java/com/panda/controller/UserController.java
+++ src/main/java/com/panda/controller/UserController.java
@@ -3,6 +3,7 @@ package com.panda.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,10 +12,12 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("user")
 public class UserController {
 
+	@Value("${jdbc.url}")
+	private String jdbcUrl;
+	
 	@RequestMapping("/show")
 	public ModelAndView show(){
 		ModelAndView view = new ModelAndView("index");
-		
 		List<String> userList = new ArrayList<>();
 		userList.add("李磊");
 		userList.add("李白");
