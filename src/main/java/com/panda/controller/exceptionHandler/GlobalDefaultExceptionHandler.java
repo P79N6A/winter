8cbdbe25fb500ee2786b235ebdd37345e3d1b4d8
zhanghui <zhanghui@souche.com>
2017-11-21 10:18:01 +0800

    update

--- src/main/java/com/panda/controller/exceptionHandler/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/panda/controller/exceptionHandler/GlobalDefaultExceptionHandler.java
@@ -1,23 +1,23 @@
 package com.panda.controller.exceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @ControllerAdvice
 class GlobalDefaultExceptionHandler {
     public static final String DEFAULT_ERROR_VIEW = "error";
- 
+
     @ExceptionHandler(value = Exception.class)
     public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
-        // 如果异常使用了ResponseStatus注解，那么重新抛出该异常，Spring框架会处理该异常。 
+        // 如果异常使用了ResponseStatus注解，那么重新抛出该异常，Spring框架会处理该异常。
         if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null)
             throw e;
- 
+
         // 否则创建ModleAndView，处理该异常。
         ModelAndView mav = new ModelAndView();
         mav.addObject("exception", e);
--- src/test/java/com/wuxi/JsonTest.java
+++ src/test/java/com/wuxi/JsonTest.java
@@ -1,5 +1,7 @@
 package com.wuxi;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
 
 import org.codehaus.jackson.map.type.TypeFactory;
@@ -10,5 +12,17 @@ public class JsonTest {
 	@Test
 	public void type(){
 		System.out.println(TypeFactory.type(Map.class));
+	}
+	
+	@Test
+	public void time(){
+		Calendar cal = Calendar.getInstance();
+		String fDate = "1501516800";
+		System.out.println(Long.valueOf(fDate));
+	    cal.setTimeInMillis(Long.valueOf(fDate)*1000);
+		String firstLicensePlateYear = String.valueOf(cal.get(Calendar.YEAR));
+		String firstLicensePlateMonth = String.valueOf(cal.get(Calendar.MONTH) + 1);
+		System.out.println(firstLicensePlateYear);
+		System.out.println(firstLicensePlateMonth);
 	}
 }
