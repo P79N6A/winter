330552a6514f2c9a32a0bfccb0e6a3877f98e00f
zhanghui <zhanghui@souche.com>
2018-03-05 10:12:58 +0800

    redis

--- src/main/java/com/panda/util/JsonUtil.java
+++ src/main/java/com/panda/util/JsonUtil.java
@@ -1,7 +1,14 @@
 package com.panda.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 /**
  * 
@@ -26,5 +33,14 @@ public class JsonUtil {
 	 */
 	public static <T> T fromJson(String json,Class<T> clazz){
 		return JSON.parseObject(json, clazz);
+	}
+	
+	public static <T> List<T> toBeanList(String json, Class<T> clazz) throws Exception {
+		List<T> lst = new ArrayList<T>();
+		JsonArray array = new JsonParser().parse(json).getAsJsonArray();
+		for (final JsonElement elem : array) {
+			lst.add(new Gson().fromJson(elem, clazz));
+		}
+		return lst;
 	}
 }
