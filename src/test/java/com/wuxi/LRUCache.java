6b071a975f36b25e478e5071a3ab2ebf3ccb54e4
zhanghui <2291143420@qq.com>
2018-05-11 00:31:17 +0800

    linkedhashmap

---
+++ src/test/java/com/wuxi/LRUCache.java
@@ -0,0 +1,41 @@
+package com.wuxi;
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author: 青铜
+ * @create: 2018-05-11
+ **/
+public class LRUCache<K,V> extends LinkedHashMap<K,V> {
+
+
+    public LRUCache(int initialCapacity,
+                    float loadFactor,
+                    boolean accessOrder){
+        super( initialCapacity,loadFactor,accessOrder);
+    }
+
+
+    public static void main(String[] args) {
+        LRUCache<Character,Integer> lruCache = new LRUCache<>(16,0.75f,true);
+
+        String str = "abcdefghijk";
+        for(int i=0; i< str.length(); i++){
+            lruCache.put(str.charAt(i),i);
+        }
+
+        System.out.println(lruCache.size());
+        System.out.println(lruCache);
+
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        if(size() > 6){
+            return true;
+        }
+        return false;
+    }
+}
