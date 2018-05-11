8cfc5cd90579f514003c1142ab2cc0c404793b52
qingtong <qingtong@tongbanjie.com>
2018-05-11 14:32:53 +0800

    update

--- src/test/java/com/wuxi/ShardingTest.java
+++ src/test/java/com/wuxi/ShardingTest.java
@@ -12,19 +12,22 @@ public class ShardingTest {
 
     private static int MAX_DB = 64;
 
-    private static int TABLE_MOD = 4;
+    private static int TABLE_MOD = 8;
 
     private static int DB_MOD = 64;
 
     public static void main(String[] args) {
-        System.out.println(getString("000000246", "xx_"));
+        for (int i=0; i<1000;i++){
+            System.out.println(i+" : "+getString(i+"", "xx_"));
+        }
+
     }
 
     private static String getString(String value, String preTable) {
 
         long valueLong = Long.parseLong(value);
-        long dbId = ((valueLong / MAX_TABLE) % MAX_DB) / DB_MOD * DB_MOD;
-        long tableId = ((valueLong / MAX_DB) % MAX_TABLE) / TABLE_MOD * TABLE_MOD;
+        long dbId = valueLong / MAX_TABLE % MAX_DB / DB_MOD * DB_MOD;
+        long tableId = valueLong / MAX_DB % MAX_TABLE / TABLE_MOD * TABLE_MOD;
 
         String db = StringUtils.leftPad(String.valueOf(dbId), 2, '0');
         String table = StringUtils.leftPad(String.valueOf(tableId), 2, '0');
