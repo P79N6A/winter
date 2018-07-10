5828bd4c27fd863fe9d2c0ab418095c8bfc4ee3a
zhanghui <2291143420@qq.com>
2018-07-11 02:16:07 +0800

    spring mvc 时间参数注入问题

--- src/main/java/com/panda/controller/OrderController.java
+++ src/main/java/com/panda/controller/OrderController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.lang.model.element.ExecutableElement;
 import java.net.URLDecoder;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -26,14 +27,16 @@ public class OrderController {
     @ResponseBody
     public Object add(Order order){
 
-        //当请求方法是post且时间格式为"2018/07/9 13:01:48" 时String可以转换Date
+        //当请求方法是post且时间格式为"2018/07/9 13:01:48" 或get方法http://localhost:8080/order/add?createTime=2018/09/09（encodeURI）时String可以转换Date
+        //原理：调用的 ObjectToObjectConverter,他的convert方法最后调的ctor.newInstance(source)，而Date恰好有个new Date("2018/09/09")的构造器，2018-09-09不行
+        //正经方法是在createTime字段加上注释@DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")这样get/post都可以注入时间参数
         LOGGER.info(JSON.toJSONString(order));
         return order;
     }
 
     @RequestMapping("/query")
     @ResponseBody
-    public Object query(String name){
+    public Object query(String name,Integer age){
 
         try{
             name = URLDecoder.decode(name,"utf-8");
@@ -45,11 +48,7 @@ public class OrderController {
     }
 
     public static void main(String[] args) {
-        try{
-            System.out.println(URLDecoder.decode("章三","utf-8"));
-        }catch (Exception e){
-
-        }
+        System.out.println(new Date("2018/09/09"));
     }
 
 
