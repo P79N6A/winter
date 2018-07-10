d1acfea81befa792ba2a1023cb00938de38e79f8
zhanghui <2291143420@qq.com>
2018-07-11 01:05:39 +0800

    get的queryString乱码

--- src/main/java/com/panda/controller/OrderController.java
+++ src/main/java/com/panda/controller/OrderController.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.lang.model.element.ExecutableElement;
+import java.net.URLDecoder;
+import java.util.concurrent.ExecutionException;
+
 /**
  * @author: 青铜
  * @create: 2018-07-10
@@ -31,8 +35,21 @@ public class OrderController {
     @ResponseBody
     public Object query(String name){
 
+        try{
+            name = URLDecoder.decode(name,"utf-8");
+        }catch (Exception e){
+
+        }
         LOGGER.info("name:{}",name);
         return name;
+    }
+
+    public static void main(String[] args) {
+        try{
+            System.out.println(URLDecoder.decode("章三","utf-8"));
+        }catch (Exception e){
+
+        }
     }
 
 
