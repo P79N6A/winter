c1e28c162be7cba57e3b97669cc7dec5a555faf5
qingtong <qingtong@tongbanjie.com>
2018-08-14 20:30:45 +0800

    xx

--- src/test/java/com/wuxi/RSAtest.java
+++ src/test/java/com/wuxi/RSAtest.java
@@ -17,6 +17,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ *
+ *
+ * Java加密的常用的加密算法类型有三种
+ *
+ * 1单向加密：也就是不可逆的加密，例如MD5,SHA,HMAC
+ *
+ * 2对称加密：也就是加密方和解密方利用同一个秘钥对数据进行加密和解密，例如DES，PBE等等
+ *
+ * 3非对称加密：非对称加密分为公钥和秘钥，二者是非对称的，例如用私钥加密的内容需要使用公钥来解密，使用公钥加密的内容需要用私钥来解密，DSA，RSA...
+ *
+ * 而keyGenerator,KeyPairGenerator,SecretKeyFactory的三种使用方法刚好和这三种加密算法类型对上
+ *
+ * keyGenerator：秘钥生成器，也就是更具算法类型随机生成一个秘钥，例如HMAC，所以这个大部分用在非可逆的算法中
+ *
+ * SecretKeyFactory：秘密秘钥工厂，言外之意就是需要根据一个秘密（password）去生成一个秘钥,例如DES，PBE，所以大部分使用在对称加密中
+ *
+ * KeyPairGenerator:秘钥对生成器，也就是可以生成一对秘钥，也就是公钥和私钥，所以大部分使用在非对称加密中
+ * 
  * @author: 青铜
  * @create: 2018-08-14
  **/
